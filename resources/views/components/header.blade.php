5e4e5cee1575a2acc77ff34d57b9e21c0a764ce3
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-07-26 10:47:17 +0200

    created default template

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/components/header.blade.php -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Starter Template for Bootstrap</title>
+
+<!-- Bootstrap 4 CDN -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS (se lo hai in public/css) -->
+<link href="{{ asset('css/starter-template.css') }}" rel="stylesheet">
+
+<!-- Bootstrap JS + dipendenze -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <x-header/>
+</head>
+
+<body>
+
+<x-navbar/>
+
+<main role="main" class="container">
+
+   @yield("content")
+
+</main><!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- Bootstrap JS (Bundle = Popper + JS) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
