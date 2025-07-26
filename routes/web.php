c380231357b87a0f56ba871c7718b97e427ef176
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-07-26 11:18:59 +0200

    created dashboard template

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('default');
+    return view('templates.dashboard');
 });
 
 Route::get('/prova-db', function () {
