6c8eb4199fcacd94739be9cc914a40737af99bef
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-07-27 18:00:50 +0200

    used modal for full screen image when is clicked

--- resources/views/spotted/spotted.blade.php
+++ resources/views/spotted/spotted.blade.php
@@ -28,7 +28,9 @@
                             <th scope="row">{{ $loop->iteration }}</th>
                             <td>{{ $spot->data }}</td>
                             <td>
-                                <img src="https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/{{ $spot->image_path }}" width="60" height="60" alt="img">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#imageModal" data-img="https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/{{ $spot->image_path }}">
+                                    <img src="https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/{{ $spot->image_path }}" width="60" height="60" alt="img" style="cursor:pointer;">
+                                </a>
                             </td>
                             <td>{{ $spot->comment }}</td>
                             <td>{{ $spot->category }}</td>
@@ -57,4 +59,39 @@
             </div>
         </div>
     </div>
+
+
+
+
+    <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg"> <!-- larghezza maggiore -->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="imageModalLabel">Immagine</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi"></button>
+                </div>
+                <div class="modal-body d-flex justify-content-center">
+                    <img id="modalImage" src="" alt="Immagine grande" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var imageModal = document.getElementById('imageModal')
+            imageModal.addEventListener('show.bs.modal', function (event) {
+                var trigger = event.relatedTarget
+                var imgSrc = trigger.getAttribute('data-img')
+                var modalImage = imageModal.querySelector('#modalImage')
+                modalImage.src = imgSrc
+            })
+        })
+    </script>
 @endsection
+
+
+
+
+
