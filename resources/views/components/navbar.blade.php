dc54abd40c79d18143639673a7cbecb703ea7016
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-08-03 16:42:53 +0200

    group admin links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <a class="navbar-brand" href="#">Expand at sm</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03"
+            aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -16,13 +17,23 @@
                 <a class="nav-link" href="{{route("reports.index")}}">Reports</a>
             </li>
             @if(Auth::user()->isAdmin)
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route("admin.create")}}">Registra utente</a>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown03" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">Admin</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown03">
+
+                        <a class="dropdown-item" href="{{route("admin.create")}}">Registra utente</a>
+
+                        <a class="dropdown-item" href="{{route("admin.index")}}">Visualizza utenti</a>
+
+                    </div>
                 </li>
 
             @endif
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{Auth::user()->email}}</a>
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown03" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">{{Auth::user()->email}}</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown03">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
